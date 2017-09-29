34cd4429ff3b0143c0879bfcd10b01241b6044d9
alokps <alokps.ece@gmail.com>
2017-09-28 20:58:42 -0400

    Initial Commit

---
+++ sample.py
@@ -0,0 +1,27 @@
+
+class Sample:
+
+    def __init__(self):
+        self.stack_list = []
+
+    def push_value(self, value):
+        self.stack_list.append(value)
+
+    def is_empty(self):
+        return self.stack_list == []
+
+    def pop_value(self):
+        return self.stack_list.pop()
+
+    def print_stack_value(self):
+        for x in self.stack_list:
+            print(x)
+
+if __name__ == '__main__':
+    instance = Sample()
+    print(instance.is_empty())
+    instance.push_value(10)
+    instance.push_value(20)
+    instance.push_value(30)
+    print(instance.pop_value())
+    instance.print_stack_value()
