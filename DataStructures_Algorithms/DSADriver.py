3404d65ef15d70f8ad2a39223559e2a71940acff
alokps <alokps.ece@gmail.com>
2017-10-01 20:45:19 -0400

    Moved Code to Package, Commit of DataStructure Package
    Signed-off-by: alokps <alokps.ece@gmail.com>

---
+++ DataStructures_Algorithms/DSADriver.py
@@ -0,0 +1,8 @@
+from DataStructures_Algorithms.Stack import Stack
+
+if __name__ == '__main__':
+    instance = Stack()
+    print(instance.is_empty)
+    instance.push(10)
+    instance.push(20)
+    print(instance.stack_length)
